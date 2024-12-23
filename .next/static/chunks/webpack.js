31f87dc88b940e0b16db05caf3cbe78f00e7a45d
Ezequiel Santana <ezequielssantana225@gmail.com>
2024-12-22 22:25:35 -0300

    modified Home

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3e15130a0ead7e1a"; }
+/******/ 		__webpack_require__.h = function() { return "12710a4b3e914a19"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home () {
-    return <h1>Teste</h1>
+    return<><><h1>Amor da minha vida s2</h1>
+        
+        <p>Sabia que você é a coisa mais linda do mundo? sei que será uma boa mãe!!</p></>
+        
+        <h2>Vai lá amanhã ver a chibatona do menino kkkkkkk</h2></>
+    
 }
 
 export default Home;
