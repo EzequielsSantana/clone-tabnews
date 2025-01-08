5d160d37e06280f0234449cdbb957617c51a1d57
Ezequiel Santana <ezequielssantana225@gmail.com>
2025-01-07 23:04:30 -0300

    Modificando o index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 function Home () {
-    return<><><h1>Amor da minha vida s2</h1>
+    return<><> 
+        <h1>Amor da minha vida s2</h1> 
+            <p>Sabia que você é a coisa mais linda do mundo? sei que será uma boa mãe!!</p></>   
         
-        <p>Sabia que você é a coisa mais linda do mundo? sei que será uma boa mãe!!</p></>
+        <h2>O que será? uma menininha ou um garotão ?</h2>
+            <p>O que vier iremos amar do fundo de nosso corações sem dúvidas!!</p>
         
-        <h2>Vai lá amanhã ver a chibatona do menino kkkkkkk</h2></>
-    
+        <h2>Saiba que amo  você s2</h2>
+            
+    </>    
 }
 
 export default Home;
