b58a9e8ee457908a4c782451068bc58e72198320
Ezequiel Santana <ezequielssantana225@gmail.com>
2025-01-12 21:24:59 -0300

    corrigindo as frases na página inicial

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ function Home () {
         <h1>Amor da minha vida s2</h1> 
             <p>Sabia que você é a coisa mais linda do mundo? sei que será uma boa mãe!!</p></>   
         
-        <h2>O que será? uma menininha ou um garotão ?</h2>
-            <p>O que vier iremos amar do fundo de nossos corações sem dúvidas!!</p>
+        <h2>O que será? uma menininha ou um garotão?</h2>
+            <p>O que vier iremos amar do fundo de nossos corações, sem dúvidas!!</p>
         
         <h2>Saiba que amo  você s2</h2>
             
