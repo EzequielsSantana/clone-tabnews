be3c1151e8864a24461ea6c99d253a06b574c3d2
Ezequiel Santana <ezequielssantana225@gmail.com>
2025-03-24 22:45:24 -0300

    adds `/api/v1/migrations` endpoint

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "add91f8456522ee1"; }
+/******/ 		__webpack_require__.h = function() { return "85841c5f48fc07ce"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
